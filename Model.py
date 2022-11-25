a6c5eb9740297e0e9c3e7e26178d6d7dbde46b08
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-11-25 19:00:34 +0200

    initialized weights

--- Model.py
+++ Model.py
@@ -1,13 +1,19 @@
 import numpy as np
 
+Weights = []
+
 
 def Train(X, Y, number_of_hidden_layers, number_of_neurons, learning_rate, number_of_epochs, bias_value,
           activation_function):
-    print(X)
-    print(Y)
-    print(number_of_hidden_layers)
-    print(number_of_neurons)
-    print(learning_rate)
-    print(number_of_epochs)
-    print(bias_value)
-    print(activation_function)
+    initializeWeight(number_of_hidden_layers, number_of_neurons)
+
+
+def initializeWeight(number_of_hidden_layers, number_of_neurons):
+    number_of_neurons.insert(0, 5)  # 5 neurons (input Layer)
+    number_of_neurons.append(3)  # 3 neurons (output Layer)
+    for i in range(number_of_hidden_layers + 1):
+        Weights.append(np.random.rand(number_of_neurons[i + 1], number_of_neurons[i]))
+
+
+def feedForward():
+    print('hello world')
