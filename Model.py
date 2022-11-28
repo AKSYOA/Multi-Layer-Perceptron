9a0c1471aabb9f27c3986ec9d01b4dd9c1907440
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-11-28 03:57:00 +0200

    Split input into traint and test samples

--- Model.py
+++ Model.py
@@ -24,13 +24,19 @@ def clearLists():
 def Train(X, Y, number_of_hidden_layers, number_of_neurons, learning_rate, number_of_epochs, bias_value,
           activation_function_type):
     initializeWeight(number_of_hidden_layers, number_of_neurons)
+
+    X_Train = np.vstack((X[:30], X[50:80], X[100:130]))
+    Y_Train = np.vstack((Y[:30], Y[50:80], Y[100:130]))
+
+    X_Test = np.vstack((X[30:50], X[80:100], X[130:150]))
+    Y_Test = np.vstack((Y[30:50], Y[80:100], Y[130:150]))
     for i in range(number_of_epochs):
-        for j in range(X.shape[0]):
-            feedForward(X[j], number_of_hidden_layers, activation_function_type)
-            backPropagate(Y[j], number_of_hidden_layers, number_of_neurons)
-            updateWeights(X[j], number_of_hidden_layers, learning_rate)
+        for j in range(X_Train.shape[0]):
+            feedForward(X_Train[j], number_of_hidden_layers, activation_function_type)
+            backPropagate(Y_Train[j], number_of_hidden_layers, number_of_neurons)
+            updateWeights(X_Train[j], number_of_hidden_layers, learning_rate)
             clearLists()
-    TrainAccuracy(X, Y, number_of_hidden_layers, activation_function_type)
+    TestAccuracy(X_Test, Y_Test, number_of_hidden_layers, activation_function_type)
 
 
 def initializeWeight(number_of_hidden_layers, number_of_neurons):
@@ -80,13 +86,13 @@ def updateWeights(X_sample, number_of_hidden_layers, learning_rate):
         if i == 0:
             Weights[0] = np.add(Weights[0], (learning_rate * errors[0] * X_sample))
         else:
-            Weights[i] = np.add(Weights[i], (learning_rate * errors[i] * nodes_output[i]))
+            rows = nodes_output[i - 1].shape[0]
+            cols = nodes_output[i - 1].shape[1]
+            Weights[i] = np.add(Weights[i], (learning_rate * errors[i] * nodes_output[i - 1].reshape(cols, rows)))
 
 
-def TrainAccuracy(X, Y, number_of_hidden_layers, activation_function_type):
-    nodes_output.clear()
-    for i in range(X.shape[0]):
-        print(i)
-        feedForward(X[i], number_of_hidden_layers, activation_function_type)
-        print(nodes_output[-1])
+def TestAccuracy(X_test, Y_test, number_of_hidden_layers, activation_function_type):
+    for i in range(X_test.shape[0]):
         nodes_output.clear()
+        feedForward(X_test[i], number_of_hidden_layers, activation_function_type)
+        print(nodes_output[-1])
